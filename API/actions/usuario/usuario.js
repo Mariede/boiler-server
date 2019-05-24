0ffb9a65acf90c51cd644a473fbe5696b882afbf
Michel <miriede@gmail.com>
2019-05-24 02:12:20 -0300

    testes;

--- API/actions/usuario/usuario.js
+++ API/actions/usuario/usuario.js
@@ -33,10 +33,14 @@ const inserir = async (req, res) => {
 			id = req.params.id;
 
 
-
 const uploader = require('@serverRoot/helpers/uploader');
-return await uploader.execute(req, res, [['arquivoDeSubida', 2]]);
+let result = {};
 
+// Sempre array para passa nome do campo
+// true para diskStorage | false para memoryStorage (default: true)
+// se memoryStorage selecionado, utilizar buffer.toString('utf8') para converter valor da memoria
+result = await uploader.execute(req, res, [{ name: 'arquivoDeSubida' }, { name: 'arquivoDeSubida1' }], true);
+return result;
 
 
 // **** testes email
