6cb17e9d1dfb2e0303d59fc25dbae46da101efc2
Michel <miriede@gmail.com>
2019-05-24 22:05:44 -0300

    Corrigido crash na aplicação para erros gerados dentro de callbacks na lib uploader; Validação das extensões melhorada, inclui validação por MIME Types mais específica em lib uploader;

--- API/helpers/uploader.js
+++ API/helpers/uploader.js
@@ -25,37 +25,45 @@ const push = async (req, res, fileNames, extraPath, maxFileUploads = Infinity, s
 	try {
 		const diskStorage = multer.diskStorage({
 			destination: (req, file, callback) => {
-				let filePath = __serverRoot + __serverConfig.server.fileUpload.path;
+				try {
+					let filePath = __serverRoot + __serverConfig.server.fileUpload.path;
 
-				if (storageToDisk) {
-					if (!fs.existsSync(filePath)) {
-						fs.mkdirSync(filePath);
-					}
+					if (storageToDisk) {
+						if (!fs.existsSync(filePath)) {
+							fs.mkdirSync(filePath);
+						}
 
-					if (extraPath) {
-						extraPath.replace(/[|&;$%@"<>()+,]/g, '').split(/[\\/]/).forEach(
-							e => {
-								filePath = path.join(filePath, e);
+						if (extraPath) {
+							extraPath.replace(/[|&;$%@"<>()+,]/g, '').split(/[\\/]/).forEach(
+								e => {
+									filePath = path.join(filePath, e);
 
-								if (!fs.existsSync(filePath)) {
-									fs.mkdirSync(filePath);
+									if (!fs.existsSync(filePath)) {
+										fs.mkdirSync(filePath);
+									}
 								}
-							}
-						);
+							);
+						}
 					}
-				}
 
-				callback(null, filePath);
+					callback(null, filePath);
+				} catch(err) {
+					callback(err);
+				}
 			},
 			filename: (req, file, callback) => {
-				let fileOriginalName = file.originalname,
-					findLastDot = fileOriginalName.lastIndexOf('.'),
-					dateNow = (new Date()).toISOString().split('T'),
-					dateLeft = (dateNow[0] || '').replace(/-/g, ''),
-					dateRight = (dateNow[1] || '').replace(/[:]/g, '').substr(0, 6),
-					fileName = fileOriginalName.substring(0, (findLastDot !== -1 ? findLastDot : fileOriginalName.length)) + '-' + dateLeft + dateRight + (findLastDot !== -1 ? fileOriginalName.substr(findLastDot) : '');
+				try {
+					let fileOriginalName = file.originalname,
+						findLastDot = fileOriginalName.lastIndexOf('.'),
+						dateNow = (new Date()).toISOString().split('T'),
+						dateLeft = (dateNow[0] || '').replace(/-/g, ''),
+						dateRight = (dateNow[1] || '').replace(/[:]/g, '').substr(0, 6),
+						fileName = fileOriginalName.substring(0, (findLastDot !== -1 ? findLastDot : fileOriginalName.length)) + '-' + dateLeft + dateRight + (findLastDot !== -1 ? fileOriginalName.substr(findLastDot) : '');
 
-				callback(null, fileName);
+					callback(null, fileName);
+				} catch(err) {
+					callback(err);
+				}
 			}
 		});
 
@@ -65,14 +73,37 @@ const push = async (req, res, fileNames, extraPath, maxFileUploads = Infinity, s
 					const upload = multer(
 						{
 							fileFilter: (req, file, callback) => {
-								let fileTypes = new RegExp(__serverConfig.server.fileUpload.allowedExtensions, 'i'),
-									mimeType = fileTypes.test(file.mimetype),
-									extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+								try {
+									const checkExtensions = [ ...new Set((__serverConfig.server.fileUpload.allowedExtensions || '').split('|').map(
+										item => {
+											return item.substr(0, (item.indexOf(':') !== -1 ? item.indexOf(':') : item.length)).trim();
+										}
+									))].filter(
+										item => {
+											return item !== '';
+										}
+									); // new Set para valores unicos e nao vazios, assim podemos repetir extensoes para eventuais novos MIME Types
 
-								if (mimeType && extName) {
-									return callback(null, true);
-								} else {
-									callback('Uploader: Upload de arquivos apenas suporta as seguintes extensões - ' + __serverConfig.server.fileUpload.allowedExtensions);
+									const checkMimeTypes = [ ...new Set((__serverConfig.server.fileUpload.allowedExtensions || '').split('|').map(
+										item => {
+											return item.substr((item.indexOf(':') !== -1 ? item.indexOf(':') + 1 : '')).trim();
+										}
+									))].filter(
+										item => {
+											return item !== '';
+										}
+									); // new Set para valores unicos e nao vazios, assim podemos repetir extensoes para eventuais novos MIME Types
+
+									let extName = new RegExp('^(' + checkExtensions.join('|') + '){1}$', 'i').test(path.extname(file.originalname).toLowerCase()),
+										mimeType = new RegExp('^(' + checkMimeTypes.join('|') + '){1}$', 'i').test(file.mimetype);
+
+									if (extName && mimeType) {
+										return callback(null, true);
+									} else {
+										callback('Uploader: Upload de arquivos apenas suporta as seguintes extensões - ' + checkExtensions.join(', ') + ' com seus respectivos MIME Types - ' + checkMimeTypes.join(', ') + '...');
+									}
+								} catch(err) {
+									callback(err);
 								}
 							},
 							storage: (storageToDisk ? diskStorage : multer.memoryStorage()),
