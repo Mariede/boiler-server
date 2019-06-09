a104ba803a0f676d122ac3997d8df3cd8d0002fb
Michel <miriede@gmail.com>
2019-06-08 22:30:59 -0300

    http-proxy será redirecionado para a pasta de proxy; Aplicação para converte um projeto node em serviço no windows;

---
+++ _node_as_a_service/config.js
@@ -0,0 +1,7 @@
+const config = {
+	name: 'NODE-AS-A-SERVICE', // Nome do servico
+	description: 'Serviço de teste NODE TEST', // Descricao que vai aparecer no Gerenciamento de serviço do Windows
+	script: 'C:\\project-path\\API\\app.js' //caminho absoluto do script ou aplicacao
+}
+
+module.exports = config;
---
+++ _node_as_a_service/serviceOn.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// -------------------------------------------------------------------------
+// Modulos de inicializacao
+const service = require('node-windows').Service;
+const config = require('./config');
+// -------------------------------------------------------------------------
+
+// Criando um novo objeto do Serviço
+const svc = new service(
+	{
+		name: config.name,
+		description: config.description,
+		script: config.script
+	}
+);
+
+svc.on(
+	'install',
+	() => {
+		svc.start();
+		console.log('Install complete.');
+	}
+);
+
+svc.on(
+	'alreadyinstalled',
+	() => {
+		console.log('Already Installed.');
+	}
+);
+
+svc.on(
+	'start',
+	() => {
+	console.log(`${svc.name} started!`);
+});
+
+// instalando o servico
+svc.install();
