3a6ca9ae56c8d58ed6801ba7d234f1cbb357f4a5
Michel <miriede@gmail.com>
2019-07-30 01:40:29 -0300

    Retirada a validaçãode  apenas alfanuméricos do login (estamos usando email no front-end para testes);

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -24,26 +24,22 @@ const login = async (req, res) => {
 				pass = req.body.pass;
 
 			if (!validator.isEmpty(login)) {
-				if (validator.isAlphaNumeric(login)) {
-					if (!validator.isEmpty(pass)) { // Inicia a sessao
-						sess[sessWraper] = {};
+				if (!validator.isEmpty(pass)) { // Inicia a sessao
+					sess[sessWraper] = {};
 
 
 
-						/* login process - ACESSO AO DB */
-						sess[sessWraper].id = 1;
-						sess[sessWraper].login = login.trim();
-						sess[sessWraper].senhaHash = await cryptoHash.hash(pass, '123Abc');
-						sess[sessWraper].permissoes = ['LST_INFO1', 'EDT_INFO1', 'EXC_INFO2', 'LST_INFO3'];
-						/* login process - ACESSO AO DB */
+					/* login process - ACESSO AO DB */
+					sess[sessWraper].id = 1;
+					sess[sessWraper].login = login.trim();
+					sess[sessWraper].senhaHash = await cryptoHash.hash(pass, '123Abc');
+					sess[sessWraper].permissoes = ['LST_INFO1', 'EDT_INFO1', 'EXC_INFO2', 'LST_INFO3'];
+					/* login process - ACESSO AO DB */
 
 
 
-					} else {
-						errWrapper.throwThis('AUTH', 400, 'Favor preencher a senha...');
-					}
 				} else {
-					errWrapper.throwThis('AUTH', 400, 'Login deve conter um valor alfanumérico, underscore ou espaço...');
+					errWrapper.throwThis('AUTH', 400, 'Favor preencher a senha...');
 				}
 			} else {
 				errWrapper.throwThis('AUTH', 400, 'Favor preencher o login...');
