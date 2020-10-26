35729afef247f91cf9ea0510be233d8e002e9781
Michel <mariede@sorocaba.sp.gov.br>
2020-10-26 14:23:29 -0300

    Melhorias no bat de build/deploy docker;

--- _tools/final-build-deploy.bat
+++ _tools/final-build-deploy.bat
@@ -1,15 +1,5 @@
 @echo OFF
 
-cls
-echo/
-choice /n /c:YN /m "Really start this script? [Y|N]"
-echo/
-
-IF ERRORLEVEL ==2 GOTO END_SCRIPT
-IF ERRORLEVEL ==1 GOTO START_SCRIPT
-
-:START_SCRIPT
-
 set PATH_HOME_BACK_END=C:\_des\_pessoal\full-stack\boiler-server
 set PATH_HOME_FRONT_END=C:\_des\_pessoal\full-stack\boiler-react
 
@@ -19,8 +9,18 @@ set DOCKER_CONTAINER_PORTS=-p 80:4000 -p 443:5000 -p 5001:5001
 set DOCKER_CONTAINER_MEMORY=8192m
 set DOCKER_VOLUME_MOUNT=C:/Users/mariede/.docker/boiler-server
 
+cls
 echo/
-echo Iniciando script...
+choice /n /c:YN /m "Start script to build application? [Y|N]"
+echo/
+
+IF ERRORLEVEL ==2 GOTO CHECK_DEPLOY_SCRIPT
+IF ERRORLEVEL ==1 GOTO START_BUILD_SCRIPT
+
+:START_BUILD_SCRIPT
+
+echo/
+echo Iniciando build final...
 echo/
 
 if not exist "%PATH_HOME_BACK_END%\package.json" (
@@ -41,7 +41,7 @@ if not exist "%PATH_HOME_FRONT_END%\package.json" (
 
 echo/
 echo ======================================================
-echo  1-5 GENERATE FRONT-END BUILD                        #
+echo  1-3 GENERATE FRONT-END BUILD                        #
 echo ======================================================
 echo/
 
@@ -56,7 +56,7 @@ IF ERRORLEVEL 1 GOTO ERROR
 
 echo/
 echo ======================================================
-echo  2-5 GENERATE BACK-END BUILD                         #
+echo  2-3 GENERATE BACK-END BUILD                         #
 echo ======================================================
 echo/
 
@@ -71,7 +71,7 @@ IF ERRORLEVEL 1 GOTO ERROR
 
 echo/
 echo ======================================================
-echo  3-5 PACKING FINAL BUILD IN STATIC PUBLIC FOLDER     #
+echo  3-3 PACKING FINAL BUILD IN STATIC PUBLIC FOLDER     #
 echo ======================================================
 echo/
 
@@ -82,17 +82,55 @@ xcopy "%PATH_HOME_FRONT_END%\build\" "%PATH_HOME_BACK_END%\build\views\client-si
 IF ERRORLEVEL 1 GOTO ERROR
 
 echo/
+echo Procedimento bem sucedido, build final criada!!!
+echo/
+
+:CHECK_DEPLOY_SCRIPT
+
+echo/
+choice /n /c:YN /m "Start script to deploy application? [Y|N]"
+echo/
+
+IF ERRORLEVEL ==2 GOTO END_SCRIPT
+IF ERRORLEVEL ==1 GOTO START_DEPLOY_SCRIPT
+
+:START_DEPLOY_SCRIPT
+
+echo/
+echo Iniciando deploy...
+echo/
+
+if not exist "%PATH_HOME_BACK_END%\build\package.json" (
+	echo/
+	echo Arquivo package.json na pasta definida para o build final nao encontrado, terminando o processo...
+	echo/
+
+	GOTO ERROR
+)
+
+if not exist "%PATH_HOME_BACK_END%\build\views\client-side\public\config.json" (
+	echo/
+	echo Arquivo config.json - front-end - nao encontrado, terminando o processo...
+	echo/
+
+	GOTO ERROR
+)
+
+echo/
 echo ======================================================
-echo  4-5 GENERATE DOCKER IMAGE BUILD                     #
+echo  1-2 GENERATE DOCKER IMAGE BUILD                     #
 echo ======================================================
 echo/
+
+cd %PATH_HOME_BACK_END%
+IF ERRORLEVEL 1 GOTO ERROR
 
 call docker build -t %DOCKER_IMAGE_NAME% ./
 IF ERRORLEVEL 1 GOTO ERROR
 
 echo/
 echo ======================================================
-echo  5-5 GENERATE DOCKER CONTAINER BUILD                 #
+echo  2-2 GENERATE DOCKER CONTAINER BUILD                 #
 echo ======================================================
 echo/
 
@@ -100,7 +138,7 @@ call docker run -d %DOCKER_CONTAINER_PORTS% --name %DOCKER_CONTAINER_NAME% --res
 IF ERRORLEVEL 1 GOTO ERROR
 
 echo/
-echo Script bem sucedido, docker container criado!!!
+echo Procedimento bem sucedido, docker image e container criados!!!
 echo/
 
 GOTO END_SCRIPT
@@ -113,4 +151,4 @@ echo/
 
 :END_SCRIPT
 
-pause
+pause >nul
