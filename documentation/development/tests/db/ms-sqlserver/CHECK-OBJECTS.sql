a2cd41143391826823b3763513b1ee698165fc73
Michel <mariede@sorocaba.sp.gov.br>
2020-11-30 15:05:17 -0300

    Atualizações scripts sql e docker - node version;

--- documentation/development/tests/db/ms-sqlserver/CHECK-OBJECTS.sql
+++ documentation/development/tests/db/ms-sqlserver/CHECK-OBJECTS.sql
@@ -1,26 +1,34 @@
-USE NODE_TEST
+USE [NODE_TEST]
 GO
 
 --QTD OBJETOS NO ESQUEMA
 (
-Select		o.name
-			,o.schema_id
-			,o.type
-			,o.type_desc
-			,o.create_date
-			,o.modify_date
-From		sys.objects o
-Where		schema_name(o.schema_id)		Like 'nodetest%'
+Select
+	o.name
+	,o.schema_id
+	,o.type
+	,o.type_desc
+	,o.create_date
+	,o.modify_date
+From
+	sys.objects o
+Where
+	schema_name(o.schema_id) Like 'nodetest%'
 Union
-Select		i.name
-			,o.schema_id
-			,o.type
-			,o.type_desc
-			,o.create_date
-			,o.modify_date
-From		sys.objects o
-			Inner Join sys.indexes i	On(o.object_id = i.object_id)
-Where		schema_name(o.schema_id)		Like 'nodetest%'
+Select
+	i.name
+	,o.schema_id
+	,o.type
+	,o.type_desc
+	,o.create_date
+	,o.modify_date
+From
+	sys.objects o
+	Inner Join sys.indexes i
+		On(o.object_id = i.object_id)
+Where
+	schema_name(o.schema_id) Like 'nodetest%'
 )
-Order by	type_desc
-			,name;
+Order by
+	type_desc
+	,name;
