286a8a1ed6d42bf649dc86993fc5d335d191e2d2
Michel <miriede@gmail.com>
2021-01-17 17:23:15 -0300

    Atualizações na criação da build e deploy;

--- _tools/final-build-deploy.bat
+++ _tools/final-build-deploy.bat
@@ -1,10 +1,11 @@
 @echo OFF
 
-set PATH_HOME=C:\_des\_pessoal\full-stack
+set PATH_HOME=C:\_Projetos\_full-stack
 set PATH_HOME_BACK_END=%PATH_HOME%\boiler-server
 set PATH_HOME_FRONT_END=%PATH_HOME%\boiler-react
 
 set DOCKER_WEB_IMAGE_NAME=boiler-deploy:1.0.0
+set DOCKER_WEB_IMAGE_SAVED=boiler-deploy-1.0.0
 set DOCKER_WEB_CONTAINER_NAME=boiler-server
 set DOCKER_WEB_CONTAINER_MEMORY=8192m
 set DOCKER_WEB_VOLUME_MOUNT=C:/Users/mariede/.docker/_web/boiler-server
@@ -128,12 +129,18 @@ echo/
 cd %PATH_HOME_BACK_END%
 IF ERRORLEVEL 1 GOTO ERROR
 
-call docker-compose up -d --force-recreate
+call docker-compose build --no-cache --pull
 IF ERRORLEVEL 1 GOTO ERROR
+
+call docker save -o "%PATH_HOME%\images\%DOCKER_WEB_IMAGE_SAVED%.tar" %DOCKER_WEB_IMAGE_NAME%
+IF ERRORLEVEL 1 GOTO ERROR
+
+REM call docker-compose up -d --force-recreate
+REM IF ERRORLEVEL 1 GOTO ERROR
 
 echo/
 echo ================================================================
-echo  - Procedimento bem sucedido, imagem / containers criados !!!  #
+echo  - Procedimento bem sucedido, imagem criada e salva !!!        #
 echo ================================================================
 echo/
 
