be69a8ee52eeb747b4fa2fdf0c160d33c6ed75e8
Michel <mariede@sorocaba.sp.gov.br>
2021-12-23 13:01:09 -0300

    ** packages update;

--- .webpack.config.js
+++ .webpack.config.js
@@ -115,7 +115,8 @@ const generateBuild = {
 	resolve: {
 		alias: {
 			'@serverRoot': sourcePath
-		}
+		},
+		fullySpecified: false
 	}
 };
 // ------------------------------------------------------------------------------
